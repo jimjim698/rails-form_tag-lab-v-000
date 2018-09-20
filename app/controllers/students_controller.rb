7a99b1ed0fca0bd6b15cef62d7972fa92200b778
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-20 20:51:10 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student.save 
   end 
 
 end
